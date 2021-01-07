925db028e3a70a2c46473ea9b241bc0e51be6c3a
ecl5pse <fantamas20@gmail.com>
2021-01-07 02:12:02 +0200

    add vue.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,108 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="icons">
+      <img src="../assets/logo.png" />
+    </div>
+    <h1 class="headerText">Test</h1>
+    <div class="cardt">
+      <h2 class="title">О нас</h2>
+      <p class ="text">
+        Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+        вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на
+        латинице с начала XVI века. В то время некий безымянный печатник создал
+        большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для
+        распечатки образцов. Lorem Ipsum не только успешно пережил без заметных
+        изменений пять веков, но и перешагнул в электронный дизайн. Его
+        популяризации в новое время послужили публикация листов Letraset с
+        образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы
+        электронной вёрстки типа Aldus PageMaker, в шаблонах которых
+        используется Lorem Ipsum
+      </p>
+    </div>
+    
+  <div class="box"></div>
+    <div class="box"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(200, 238, 238, 0.993);
+  max-width: 100%;
+  height: 300px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+img {
+  padding: 5px;
+  max-height: 50px;
+  max-width: 50px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.headerText {
+  font-family: "Staatliches", cursive;
+  padding: 5px;
+  text-align: center;
+  color: orangered;
+  font-size: 40px;
 }
-a {
-  color: #42b983;
+
+.cardt{
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  border-radius: 3%;
+  background-color: white;
+  -webkit-box-shadow: 0px 10px 26px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 10px 26px 0px rgba(0,0,0,0.75);
+  box-shadow: 0px 10px 26px 0px rgba(0,0,0,0.75);
+  margin-top: 25px;
+}
+
+.title{
+  color: orangered;
+  font-family: "Staatliches", cursive;
+  font-size: 30px;  
+}
+
+.text{
+  margin-top: -10px;
+  font-family: 'Comfortaa', cursive;
+  font-size: 15px;
+}
+
+.box{
+  padding: 10px;
+  width:100px ;
+  min-height: 100px;
+  margin: 1rem;
+  animation: test  10s infinite;
+}
+
+@keyframes test {
+ 0% {
+    background-image: url('../assets/img1.jpg');
+    background-repeat: no-repeat;
+       width: 100px;
+          height: 100px;
+
+  }
+  100% {
+        background-image: url('../assets/img2.jpg');
+         background-repeat: no-repeat;
+         width: 100px;
+          height: 100px;
+  }
 }
 </style>
