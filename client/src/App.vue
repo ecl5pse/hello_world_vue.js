50a8f0453871e830dbbee91e4356409f07be28b2
ecl5pse <fantamas20@gmail.com>
2021-01-08 15:19:14 +0200

    add docker(not work)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Page/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Page from './components/Page.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Page
   }
 }
 </script>
